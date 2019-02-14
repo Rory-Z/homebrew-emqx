7228af431873c50b7421c6b9aca1ffc18a632f31
zhanghongtong <zhanghongtong@foxmail.com>
2019-02-14 18:24:22 +0800

    1

--- Formula/emqx.rb
+++ Formula/emqx.rb
@@ -3,6 +3,14 @@ class Emqx < Formula
   url "https://github.com/emqx/emqx/releases/download/v3.0-rc.5/emqx-macosx-v3.0-rc.5.zip"
 
   def install
-    system "unzip emqx-macosx-v3.0-rc.5.zip"
+    prefix.install Dir["*"]
+    bin.install Dir[libexec/"/bin/emqx"]
+    rm %W[#{bin}/emqx.cmd #{bin}/emqx_ctl.cmd]
+  end
+
+  test do
+    system emqx, "start"
+    system emqx_ctl, "status"
+    system emqx, "stop"
   end
 end
