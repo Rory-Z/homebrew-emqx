5181149b822bfa3502b5046ff3b0b6367d86f726
zhanghongtong <zhanghongtong@foxmail.com>
2019-02-14 18:38:37 +0800

    1

--- Formula/emqx.rb
+++ Formula/emqx.rb
@@ -1,6 +1,6 @@
 class Emqx < Formula
   homepage "https://emqx.io"
-  url "https://github.com/emqx/emqx/releases/download/v3.0-rc.5/emqx-macosx-v3.0-rc.5.zip"
+  url "https://github.com/emqx/emqx/releases/download/v3.0.1/emqx-macosx-v3.0.1.zip"
 
   def install
     prefix.install Dir["*"]
