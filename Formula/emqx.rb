9053209cf096bdabbafb81ed7bac5722dd8bae4a
zhanghongtong <zhanghongtong@foxmail.com>
2019-02-15 18:12:42 +0800

    1

--- Formula/emqx.rb
+++ Formula/emqx.rb
@@ -1,11 +1,12 @@
 class Emqx < Formula
   homepage "https://emqx.io"
   url "http://139.198.122.143/emqx-macos-v3.0.1.zip"
+  sha256 "da42d75bc04a79415eec179f55466d59a4b7598af16aa6c63335f9f98fa05cbe"
 
   def install
     prefix.install Dir["*"]
     bin.install Dir[libexec/"/bin/emqx"]
-    system "madir", "-p", "#{prefix}/data/configs"
+    system "mkdir", "-p", "#{prefix}/data/configs"
     rm %W[#{bin}/emqx.cmd #{bin}/emqx_ctl.cmd]
   end
 
