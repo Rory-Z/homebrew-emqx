e8012e5e79785f85b363d5019a1176862e3a2e42
zhanghongtong <zhanghongtong@foxmail.com>
2019-02-15 19:11:10 +0800

    1

--- Formula/emqx.rb
+++ Formula/emqx.rb
@@ -3,11 +3,15 @@ class Emqx < Formula
   url "http://139.198.122.143/emqx-macos-v3.0.1.zip"
   sha256 "da42d75bc04a79415eec179f55466d59a4b7598af16aa6c63335f9f98fa05cbe"
 
+  depends_on "openssl"
+
   def install
     prefix.install Dir["*"]
     bin.install Dir[libexec/"/bin/emqx"]
     rm %W[#{bin}/emqx.cmd #{bin}/emqx_ctl.cmd]
   end
+
+  plist_options :manual => "emqx"
 
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
