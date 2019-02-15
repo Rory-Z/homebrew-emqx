ed5a14c7e19e37353b9114ef6ccaed99944dce0e
zhanghongtong <zhanghongtong@foxmail.com>
2019-02-15 19:15:33 +0800

    1

--- Formula/emqx.rb
+++ Formula/emqx.rb
@@ -13,7 +13,7 @@ class Emqx < Formula
 
   plist_options :manual => "emqx"
 
-  def plist; <<-EOS.undent
+  def plist; <<-EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
     "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
